78c9359565d03b25441e7c5308ea134bb41e7406
heroq <hhero@kakao.com>
2021-12-28 17:53:14 +0900

    BOJ-27

---
+++ BOJ/4673-셀프넘버.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>	
+
+using namespace std;
+long long sum(vector<int> &a){
+	std::vector<int>::iterator iter;
+	long long result = 0;
+	for(iter = a.begin(); iter != a.end(); iter++){
+		result += *iter;
+	}
+	return result;
+}
+
+
+int main(int argc, char** argv) {
+
+	vector<int> aa;
+	aa.push_back(1);
+	aa.push_back(2);
+	aa.push_back(3);
+	int a = sum(aa);
+	cout << a; 
+
+	return 0;
+}
