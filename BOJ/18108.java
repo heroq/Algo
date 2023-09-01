2dd343b8d99f034c44fd40b9a40eff9a7bbe355c
heroq <hhero@kakao.com>
2023-09-01 09:49:02 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ BOJ/18108.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		var br = new BufferedReader(new InputStreamReader(System.in));
+		int year = Integer.parseInt(br.readLine());
+		System.out.println(year - 543);
+	}
+}
