82ac78232487cd7b57f6168bebe6a3487aac8eac
heroq <hhero@kakao.com>
2023-09-01 17:24:06 +0900

    새싹

---
+++ BOJ/25083.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws Exception{
+		String a = """
+         ,r'"7
+r`-_   ,'  ,/
+ \\. ". L_r'
+   `~\\/
+      |
+      |""";
+		System.out.print(a);
+	}
+}
+
