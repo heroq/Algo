9bc9605558d7a239109d0e413822b8a324cd1d70
heroq <96118954+heroq@users.noreply.github.com>
2023-12-11 15:10:12 +0900

    Create 10809-알파벳찾기.py

---
+++ BOJ/10809-알파벳찾기.py
@@ -0,0 +1,24 @@
+from typing import List
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+strsArr = []
+
+# 문자를 반복
+for s in strs:
+    tempArr = [s]
+    if len(strsArr) == 0:
+        strsArr.append(tempArr)
+    else:
+        # 저장된 문자를 반복
+        count = 0
+        for x in range(len(strsArr)):
+            if ''.join(sorted(strsArr[x][0])) == ''.join(sorted(s)):
+                strsArr[x].append(s)
+                count = 1
+
+        if count == 0:
+            strsArr.append(tempArr)
+
+strsArr = sorted(strsArr)
+sortStrsArr = [sorted(row) for row in strsArr]
+print(sorted(sortStrsArr, key=len))
