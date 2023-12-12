f63bf4540667471351992d2558196f4a719e6ffa
heroq <96118954+heroq@users.noreply.github.com>
2023-12-12 09:19:07 +0900

    항해99

--- Leetcode/3sum.py
+++ Leetcode/3sum.py
@@ -8,6 +8,7 @@ nums = [-1, 0, 1, 2, -1, -4]
 # 3.
 
 def threeSum(nums):
+    result = []
     nums.sort()  # 투 포인터 사용하니깐 정렬
 
     for i in range(len(nums) - 2):  # 3개 초과 할 때 시작
@@ -16,6 +17,26 @@ def threeSum(nums):
 
         left, right = i + 1, len(nums) - 1  # 투 포인터 초기화
 
-        while left < right: # right
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+
+            if total > 0:
+                left += 1
+            elif total < 0:
+                right -= 1
+            else:
+                result.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left+1]:
+                    left += 1
+
+                while left < right and nums[right] == nums[right-1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+    return result
 
 
+print(threeSum(nums))
