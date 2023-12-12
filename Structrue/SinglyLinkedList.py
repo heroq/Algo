42783449d0f8d853e0a5475d06937cf4c8f67e60
heroq <96118954+heroq@users.noreply.github.com>
2023-12-12 22:00:33 +0900

    Update SinglyLinkedList.py

--- Structrue/SinglyLinkedList.py
+++ Structrue/SinglyLinkedList.py
@@ -1,12 +1,71 @@
-# 싱글 링크드리스트 구현
-# 삽입
-# 삭제
-# 조회
+# 노드
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# 노드가 필요하다
-# 싱글은 포인터가 한개여서 단일로 하나로만 쭉 간다.
-# ㅁ - ㅁ - ㅁ - ㅁ 이면은, 0은 1을 바라보고 1은 2를 바라본다..
 
-class SinglyLinkedList(object):
+# 단일 링크드리스트
+class SinglyLinkedList:
     def __init__(self):
-        self.head = 0
+        self.head = None
+        self.tail = None
+
+    # 마지막 삽입
+    def append(self, data):
+        new_node = Node(data)  # 노드 생성
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+
+        self.tail = new_node
+
+    # 첫번째 삽입
+    def prepend(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    # 삭제
+    def remove(self, data):
+        linked = self.head
+        prev_node = None
+
+        while linked is not None:
+            if linked.data == data:
+
+                if self.head == linked:
+                    self.head = linked.next
+
+                if self.tail == linked:
+                    self.tail = prev_node
+
+                prev_node.next = linked.next
+                linked = None
+
+            prev_node = linked
+
+            if linked is not None:
+                linked = linked.next
+
+
+temp = SinglyLinkedList()
+temp.append(1)
+temp.append(2)
+temp.append(3)
+temp.prepend(99)
+temp.append(4)
+temp.prepend(98)
+temp.remove(3)
+
+iterator = temp.head
+
+while iterator is not None:
+    print(iterator.data)
+    iterator = iterator.next
