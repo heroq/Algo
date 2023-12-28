d54be2da4e0490b91b8b956dbc76ef9df643fcf7
황영웅 <96118954+heroq@users.noreply.github.com>
2023-12-29 00:59:42 +0900

    [Bronze II] Title: 블랙잭, Time: 136 ms, Memory: 110432 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,13 @@
+import sys
+from itertools import combinations
+
+n, m = map(int, sys.stdin.readline().split())
+card = list(map(int, sys.stdin.readline().split()))
+black_jack = -1
+
+a = combinations(card, 3)
+for i in a:
+    if black_jack < sum(i) <= m:
+        black_jack = sum(i)
+
+print(black_jack)
